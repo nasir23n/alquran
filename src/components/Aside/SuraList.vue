903d500e5391d8ecbd0a4789a5ac41d971fa420f
Nasrullah(nasir) <nasrullah23a@gmail.com>
2022-10-07 23:39:19 +0600

    hide button if install

--- src/components/Aside/SuraList.vue
+++ src/components/Aside/SuraList.vue
@@ -32,10 +32,10 @@ window.addEventListener("beforeinstallprompt", (e) => {
         deferredPrompt.value.userChoice.then((choiceResult) => {
             if (choiceResult.outcome === "accepted") {
                 console.log("User accepted the A2HS prompt");
-                deferredPrompt.value = null;
             } else {
                 console.log("User dismissed the A2HS prompt");
             }
+            deferredPrompt.value = null;
         });
     });
 });
@@ -47,7 +47,7 @@ window.addEventListener("beforeinstallprompt", (e) => {
         class="sura_list h-screen hidden md:block overflow-y-auto w-[300px] border-r bg-slate-100 border-slate-200 dark:border-slate-800 dark:bg-slate-800"
         :class="{'aside_open': store.state.asideOpen}"
     >
-        <button ref="addBtn" class="flex gap-2 w-full text-[18px] py-2 px-3 text-center bg-emerald-500 justify-center items-center">
+        <button v-if="modal_open" ref="addBtn" class="flex gap-2 w-full text-[18px] py-2 px-3 text-center bg-emerald-500 justify-center items-center">
             Add to homescreen
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
         </button>
